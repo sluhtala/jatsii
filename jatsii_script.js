434013ef7e37c61f18d3c288f728f54ef2464b37
Sasu Luhtala <sluhtala@me.com>
2020-04-30 15:10:01 +0300

    styling

--- jatsii_script.js
+++ jatsii_script.js
@@ -1,19 +1,23 @@
 
 var locked_dice = [0, 0, 0, 0, 0];
 var dices = [1, 1, 1, 1, 1];
-
+var roll_amount = 0;
 function lock_dice(dice_num, id)
 {
 	var dice = parseInt(dice_num);
+	if (roll_amount == 0)
+	{
+		return ;
+	}
 	if (locked_dice[dice - 1] == 0)
 	{
 		locked_dice[dice - 1] = 1;
-		document.getElementById(id).style.color = "grey";
+		document.getElementById(id).style.opacity = 0.5;
 	}
 	else
 	{
 		locked_dice[dice - 1] = 0;
-		document.getElementById(id).style.color = "black";
+		document.getElementById(id).style.opacity = 1;
 	}
 }
 
@@ -21,6 +25,8 @@ function roll_dice()
 {
 	var randomroll;
 	var i = 0;
+	roll_amount += 1;
+
 	while (i < 5)
 	{
 		if (locked_dice[i] != 1)
@@ -31,10 +37,27 @@ function roll_dice()
 		i++;
 	}
 	update_dices();
+	check_jatsii();
+}
+
+function check_jatsii()
+{
+	var num = dices[0];
+	var i = 1;
+	while (i < 5)
+	{
+		if (dices[i] != num)
+		{
+			return ;
+		}
+		i++;
+	}
+	document.getElementById("jatsii_text").innerHTML = "JATSII! " + roll_amount + " heittoa";
 }
 
 function update_dices()
 {
+	document.getElementById("roll_amount").innerHTML = "Heittojen m&#228;&#228;r&#228;: " + roll_amount;
 	document.getElementById("dice_1").innerHTML = dices[0];
 	document.getElementById("dice_2").innerHTML = dices[1];
 	document.getElementById("dice_3").innerHTML = dices[2];
@@ -51,11 +74,13 @@ function reset_all()
 		locked_dice[i] = 0;
 		i++;
 	}
-	document.getElementById("dice_1").style.color = "black";
-	document.getElementById("dice_2").style.color = "black";
-	document.getElementById("dice_3").style.color = "black";
-	document.getElementById("dice_4").style.color = "black";
-	document.getElementById("dice_5").style.color = "black";
+	roll_amount = 0;
+	document.getElementById("dice_1").style.opacity = 1;
+	document.getElementById("dice_2").style.opacity = 1;
+	document.getElementById("dice_3").style.opacity = 1;
+	document.getElementById("dice_4").style.opacity = 1;
+	document.getElementById("dice_5").style.opacity = 1;
+	document.getElementById("jatsii_text").innerHTML = "";
 	update_dices();
 }
 
